33fd0113e0e7df858cfc4954e46e2ccfdb67d422
Ronan <ronanlet@gmail.com>
2017-03-11 16:05:33 +0100

    Add: SimpleAjax v1.0

---
+++ README.md
@@ -0,0 +1,108 @@
+SimpleAjax
+==========
+
+
+**SimpleAjax** is a lightweight javascript Ajax library.
+It covers all HTTP methods and headers with an easy syntax.
+
+
+Installation
+----------------
+
+Simply include the library to your project in html and you're good to go.
+```
+<script type="text/javascript" src="/path/to/simpleajax.js"></script>
+```
+
+Usage
+---------
+
+Generic ajax request syntax
+
+```
+SimpleAjax.request('url', options)
+```
+
+You can also use `.get` `.post` `.put` `.patch` and `.delete` if you don't feel like passing the HTTP method in your options
+
+```
+SimpleAjax.get('url', options)
+SimpleAjax.post('url', options)
+SimpleAjax.put('url', options)
+SimpleAjax.patch('url', options)
+SimpleAjax.delete('url', options)
+```
+
+Options
+-----------
+
+```
+{
+  // If using .request(), specify the HTTP method
+  method: 'GET' || 'POST || 'PUT' || 'PATCH' || 'DELETE',
+  // Headers are passed as json with header name as key
+  headers: {},
+  // Data are passed as json
+  data: {},
+  // Success callback
+  success: function(response) {
+    // Manage request response here
+  },
+  // Error callback
+  error: function(err) {
+    // Manage request error here
+  }
+}
+```
+
+Examples
+------------
+
+```
+SimpleAjax.request('http://myapi.example.org/ressource', {
+  method: 'GET',
+  success: function(response) {
+    console.log('Success', response)
+  },
+  error: function(err) {
+    console.error('Error', err)
+  }
+})
+```
+
+```
+SimpleAjax.post('/path/to/login.py', {
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  data: {
+    username: 'JohnDoe',
+    password: 'pwd42'
+  },
+  success: function(response) {
+    var user = JSON.parse(response)
+    console.log(user.id)
+  },
+  error: function(err) {
+    console.error('Error', err)
+  }
+})
+```
+
+ Notes & TODO
+======================
+
+>- I will work on a version with promises
+>- Please note that this is a very small, straight to the point ajax tool. It might not be suited for complex requirements
+>- Don't hesitate to post issues & pull requests
+
+
+License
+-------
+
+SimpleAjax was conceived and programmed by Ronan Letellier
+
+SimpleAjax is released under the terms of the GNU General Public License v3
+(see LICENSE)
+Additionnaly, if we meet some day and you think this Software is worth it,
+you can buy me a beer in return. (See http://en.wikipedia.org/wiki/Beerware)
---
+++ SimpleAjax/simpleajax.js
@@ -0,0 +1,183 @@
+(function() {
+  /*
+   * Initialization
+   */
+  function SimpleAjax () {
+    this.authorizedMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    this.authorizedHeaders = [
+      'accept',
+      'accept-charset',
+      'accept-encoding',
+      'accept-language',
+      'accept-datetime',
+      'authorization',
+      'cache-control',
+      'connection',
+      'cookie',
+      'content-length',
+      'content-md5',
+      'content-type',
+      'date',
+      'expect',
+      'forwarded',
+      'from',
+      'host',
+      'if-match',
+      'if-modified-since',
+      'if-none-match',
+      'if-range',
+      'if-unmodified-since',
+      'max-forwards',
+      'origin',
+      'pragma',
+      'proxy-authorization',
+      'range',
+      'referer',
+      'te',
+      'user-agent',
+      'upgrade',
+      'via',
+      'warning',
+      'x-requested-with',
+      'dnt',
+      'x-forwarded-for',
+      'x-forwarded-host',
+      'x-forwarded-proto',
+      'front-end-https',
+      'x-http-method-override',
+      'x-att-deviceid',
+      'x-wap-profile',
+      'proxy-connection',
+      'x-uidh',
+      'x-csrf-token',
+      'x-request-id',
+      'x-correlation-id'
+    ]
+
+    this.options = {}
+    this.error = null
+    this.warnings = []
+  }
+
+  /*
+   * Public methods
+   */
+
+  // Generic ajax request, method must be passed in options
+  SimpleAjax.prototype.request = function(url, options) {
+    this.url = url
+    this.options = options
+
+    checkOptions(this)
+    execute(this)
+  }
+
+  // GET ajax request
+  SimpleAjax.prototype.get = function (url, options) {
+    options.method = 'GET'
+    this.request(url, options)
+  }
+
+  // POST ajax request
+  SimpleAjax.prototype.post = function (url, options) {
+    options.method = 'POST'
+    this.request(url, options)
+  }
+
+  // PUT ajax request
+  SimpleAjax.prototype.put = function (url, options) {
+    options.method = 'PUT'
+    this.request(url, options)
+  }
+
+  // PATCH ajax request
+  SimpleAjax.prototype.patch= function (url, options) {
+    options.method = 'PATCH'
+    this.request(url, options)
+  }
+
+  // DELETE ajax request
+  SimpleAjax.prototype.delete = function (url, options) {
+    options.method = 'DELETE'
+    this.request(url, options)
+  }
+
+  /*
+   * Private methods
+   */
+
+  // Executing ajax request if we have no error
+  function execute (instance) {
+    // Displaying errors in console
+    if (instance.error) {
+      console.error('Simple Ajax: ' + instance.error)
+      return
+    } else if (instance.warnings) {
+      // Displaying warnings in console
+      instance.warnings.forEach(function (warning) {
+        console.warn('Simple Ajax: ' + warning)
+      })
+    }
+
+    // HTTP Request
+    var xhr = new XMLHttpRequest()
+    xhr.open(instance.options.method, instance.url)
+
+    if (instance.options.headers) {
+      Object.keys(instance.options.headers).forEach(function (key) {
+        xhr.setRequestHeader(key, instance.options.headers[key])
+      })
+    }
+
+    // Callbacks
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        instance.options[xhr.status === 200 ? 'success' : 'error'](xhr.responseText || xhr)
+      }
+    }
+
+    xhr.send(instance.options.data)
+  }
+
+  // Checking options passed
+  function checkOptions (instance) {
+    // Errors
+    if (instance.options.method) {
+      instance.options.method = instance.options.method.toUpperCase()
+    }
+    if (!instance.options.method) {
+      instance.error = 'You need to specify a method in your options'
+    } else if (instance.authorizedMethods.indexOf(instance.options.method) === -1) {
+      instance.error = 'Authorized HTTP methods are: ' + instance.authorizedMethods.join(', ')
+    }
+
+    // Warnings
+    if (instance.options.headers) {
+      Object.keys(instance.options.headers).forEach(function (key) {
+        if (instance.authorizedHeaders.indexOf(key.toLowerCase()) === -1) {
+          delete instance.options.headers[key]
+          instance.warnings.push('Unknown HTTP header: ' + key + '. Header was removed from request.')
+        }
+      })
+    }
+
+    if (!instance.options.success) {
+      instance.warnings.push('No success callback specified')
+    }
+
+    if (!instance.options.error) {
+      instance.warnings.push('No error callback specified')
+    }
+
+    if (instance.options.method !== 'GET' && !instance.options.data) {
+      instance.warnings.push('No data passed in HTTP request')
+    }
+
+    if (instance.options.method === 'GET' && instance.options.data) {
+      instance.options.data = null
+      instance.warnings.push("GET method, data won't be sent")
+    }
+  }
+
+  this.SimpleAjax = new SimpleAjax()
+})()
